32a1bf56f88b5b35edbbdc4208eade494852bcc1
artur <babalasoft@gmail.com>
2020-05-22 13:15:38 +0200

    Add stubs to the client, implement PlayStream body

--- client.cc
+++ client.cc
@@ -1,20 +1,81 @@
+#include <grpcpp/grpcpp.h>
+
 #include <iostream>
 #include <memory>
 #include <string>
-
-#include <grpcpp/grpcpp.h>
 
 #include "grpcalsa.grpc.pb.h"
 
 using grpc::Channel;
 using grpc::ClientContext;
+using grpc::ClientWriter;
+using grpc::ClientReader;
 using grpc::Status;
 using grpcalsa::AudioData;
+using grpcalsa::GrpcAlsa;
 using grpcalsa::PlayStatus;
-using grpc::ClientWriter;
-using grpc::ClientContext;
+using grpcalsa::RecordRequest;
 using namespace std;
 
+class GrpcAlsaClient {
+ public:
+  GrpcAlsaClient(std::shared_ptr<Channel> channel)
+      : stub_(GrpcAlsa::NewStub(channel)) {}
+
+  int PlayStream(const char* data) {
+    // Context for the client. It could be used to convey extra information to
+    // the server and/or tweak certain RPC behaviors.
+    ClientContext context;
+    PlayStatus play_status;
+    
+    AudioData d;
+    d.set_data(data);
+    unique_ptr<ClientWriter<AudioData> > writer(
+        stub_->PlayStream(&context, &play_status));
+
+    bool s = writer->Write(d);
+    writer->WritesDone();
+    Status status = writer->Finish();
+    // Act upon its status.
+    if (status.ok()) {
+      cout << "Done streaming with status: " << play_status.status() << std::endl;
+      return 0;
+    } else {
+      std::cout << status.error_code() << ": " << status.error_message()
+                << std::endl;
+      return -1;
+    }
+  }
+
+  int RecordStream(char* data, int size) {
+    // Context for the client. It could be used to convey extra information to
+    // the server and/or tweak certain RPC behaviors.
+    ClientContext context;
+    RecordRequest record_request;
+    record_request.set_duration(size);
+    record_request.set_id(185);
+    
+    AudioData d;
+    d.set_data(data);
+    unique_ptr<ClientReader<AudioData> > reader(
+        stub_->RecordStream(&context, record_request));
+
+    bool s = reader->Read(&d);
+    Status status = reader->Finish();
+    // Act upon its status.
+    if (status.ok()) {
+      cout << "Done streaming with status: " << endl;
+      return 0;
+    } else {
+      cout << status.error_code() << ": " << status.error_message()
+                << endl;
+      return -1;
+    }
+  }
+
+ private:
+  std::unique_ptr<GrpcAlsa::Stub> stub_;
+};
 
 int main(int argc, char** argv) {
   string target_str;
@@ -37,6 +98,14 @@ int main(int argc, char** argv) {
   } else {
     target_str = "localhost:50051";
   }
-  cout << "Hello gRPC" << endl;
+  cout << "gRPC client started " << target_str << endl;
+  std::shared_ptr<Channel> channel(grpc::CreateChannel(
+      target_str, grpc::InsecureChannelCredentials()));
+  GrpcAlsaClient gac(channel);
+  gac.PlayStream("Hello World");
+  char* buffer = new char[16];
+  gac.RecordStream(buffer, sizeof(buffer));
+  
+  delete [] buffer;
   return 0;
 }
--- server.cc
+++ server.cc
@@ -14,13 +14,48 @@ using grpc::ServerBuilder;
 using grpc::ServerContext;
 using grpc::Status;
 using grpc::ServerReader;
+using grpcalsa::GrpcAlsa;
 using grpcalsa::AudioData;
 using grpcalsa::PlayStatus;
 
 using namespace std;
 
+// Logic and data behind the server's behavior.
+class GrpcAlsaServiceImpl final : public GrpcAlsa::Service {
+  Status PlayStream(ServerContext* context, 
+                    ServerReader<AudioData>* reader, 
+                    PlayStatus* response){
+    AudioData data;
+    while (reader->Read(&data)) {
+      std::cout << "read data: " << data.data() << std::endl;
+    }
+    response->set_status(77);
+    return Status::OK;
+  }
+
+};
+
 void RunServer() {
-  cout << __func__ << " gRPC" << endl;
+  cout << "gRPC" << __func__ << endl;
+
+  std::string server_address("0.0.0.0:50051");
+  GrpcAlsaServiceImpl service;
+
+  grpc::EnableDefaultHealthCheckService(true);
+  grpc::reflection::InitProtoReflectionServerBuilderPlugin();
+  ServerBuilder builder;
+  // Listen on the given address without any authentication mechanism.
+  builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+  // Register "service" as the instance through which we'll communicate with
+  // clients. In this case it corresponds to an *synchronous* service.
+  builder.RegisterService(&service);
+  // Finally assemble the server.
+  std::unique_ptr<Server> server(builder.BuildAndStart());
+  std::cout << "Server listening on " << server_address << std::endl;
+
+  // Wait for the server to shutdown. Note that some other thread must be
+  // responsible for shutting down the server for this call to ever return.
+  server->Wait();
 }
 
 int main(int argc, char** argv) {
